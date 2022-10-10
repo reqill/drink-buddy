89786c4ae118d382b16ebe57f55259c55ba11c51
Mikołaj Mrózek <m.mik311@gmail.com>
2022-10-10 21:48:50 +0200

    feat(setup): project setup

---
+++ packages/drink-buddy-ui/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextRuntimeDotenv = require('next-runtime-dotenv');
+
+const withConfig = nextRuntimeDotenv({
+  path: `${process.cwd()}/config/env/.env.${process.env.APP_ENV}`,
+  public: [
+    // TODO: add envs used by user
+  ],
+  server: [
+    // TODO: add envs used on server
+  ],
+});
+
+module.exports = withConfig({
+  swcMinify: true,
+  reactStrictMode: true,
+  images: {
+    domains: [
+      // TODO: add other domains when CD is setup
+      'localhost',
+    ],
+  },
+});
