f0bb602c8e81ae16f0779a4eeb18e49e639b5800
Mikołaj Mrózek <mikolaj.mrozek@imgarena.com>
2022-10-11 13:48:13 +0200

    feat: added daisyui

--- packages/drink-buddy-ui/pages/index.tsx
+++ packages/drink-buddy-ui/pages/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 
 const Home: NextPage = (): JSX.Element => {
   return (
-    <div className="flex w-full min-h-screen h-full justify-center align-middle">
+    <div className="flex w-full min-h-screen h-full justify-center align-middle flex-col">
       <h1 className="text-5xl font-bold m-auto pb-2 bg-gradient-to-tr bg-clip-text bg-star text-transparent from-blue-400 to-cyan-400">
         Welcome to drink-buddy
       </h1>
