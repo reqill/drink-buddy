918208f1ca2199c3257bb05b89f846ec33f458de
Mikołaj Mrózek <mikolaj.mrozek@imgarena.com>
2022-10-11 13:37:28 +0200

    feat: added tailwind

--- packages/drink-buddy-ui/pages/_app.tsx
+++ packages/drink-buddy-ui/pages/_app.tsx
@@ -1,11 +1,19 @@
 import { AppProps } from 'next/app';
+import '../styles/global.css';
 
 interface CustomPageProps {
   // custom page props
 }
 
 const App = ({ Component, pageProps }: AppProps<CustomPageProps>) => {
-  return <Component {...pageProps} />;
+  return (
+    // TODO: provider wrappers (auth, session, redux etc.)
+    <div className="bg-slate-900 min-h-screen m-0 p-0 w-screen">
+      <main className="h-full w-full min-h-screen">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 };
 
 export default App;
