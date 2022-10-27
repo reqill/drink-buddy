120fe1801c285d06d21a003ef492b210c9afbc2c
Mikołaj Mrózek <mikolaj.mrozek@imgarena.com>
2022-10-27 19:05:25 +0200

    fix: tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import { AppProps } from 'next/app';
-import '../styles/global.css';
+import '../src/styles/global.css';
 
 interface CustomPageProps {
   // custom page props
 }
 // TODO: add layouts
 
-const App = ({ Component, pageProps }: AppProps<CustomPageProps>) => {
+const App = ({ Component, pageProps }: AppProps & CustomPageProps) => {
   return (
     // TODO: provider wrappers (auth, session, redux etc.)
     <div className="bg-slate-900 min-h-screen m-0 p-0 w-screen">
