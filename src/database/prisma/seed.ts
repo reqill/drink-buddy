ee81afead19f59f82d957ae3e8658942fd95b8a6
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-11-05 10:18:06 +0100

    feat(api): read only api with sample db data
    Only list reads are possible at this moment db can be seeded with sample data using npx prisma db
    seed

--- src/database/prisma/seed.ts
+++ src/database/prisma/seed.ts
@@ -35,8 +35,82 @@ async function main() {
   await prisma.ingredient.create({
     data: {
       name: 'Vodka',
-      alcoholContent: 40.0,
+      alcoholContent: 0.4,
       categories: { connect: { name: 'Bitter' } },
+    },
+  });
+  await prisma.ingredient.create({
+    data: {
+      name: 'Orange juice',
+      alcoholContent: 0.0,
+      categories: { connect: { name: 'Sweet' } },
+    },
+  });
+  await prisma.ingredient.create({
+    data: {
+      name: 'Apple juice',
+      alcoholContent: 0.0,
+      categories: { connect: { name: 'Sweet' } },
+    },
+  });
+  await prisma.ingredient.create({
+    data: {
+      name: 'Lemon liquer',
+      alcoholContent: 0.15,
+      categories: { connect: { name: 'Sour' } },
+    },
+  });
+  await prisma.ingredient.create({
+    data: {
+      name: 'Jagermeister',
+      alcoholContent: 0.375,
+      categories: { connect: { name: 'Bitter' } },
+    },
+  });
+  await prisma.ingredient.create({
+    data: {
+      name: 'Red bull',
+      alcoholContent: 0.0,
+      categories: { connect: { name: 'Sweet' } },
+    },
+  });
+  await prisma.user.createMany({
+    data: [
+      {
+        username: 'Adam The Connoisseur',
+        email: 'TheAdam@google.com',
+        someFieldUsedForExternalAuth: 'Trust me Bro',
+      },
+      {
+        username: 'Nicolas The Normie',
+        email: 'randomguy@google.com',
+        someFieldUsedForExternalAuth: 'i do not yet have my id',
+      },
+    ],
+  });
+  await prisma.drink.create({
+    data: {
+      name: 'JagerBomb',
+      alcoholContent: 0.1,
+      volume: 400,
+      ingredientList: { connect: [{ name: 'Jagermeister' }, { name: 'Red bull' }] },
+      ingredientAmounts: ['100 ml', '300 ml'],
+      shortDescription: 'The one and only energizing drink',
+      categories: { connect: [{ name: 'Clasic' }, { name: 'Weak' }] },
+      author: { connect: { username: 'Adam The Connoisseur' } },
+    },
+  });
+  await prisma.drink.create({
+    data: {
+      name: 'Apple and orange juice',
+      alcoholContent: 0.0,
+      volume: 500,
+      ingredientList: { connect: [{ name: 'Apple juice' }, { name: 'Orange juice' }] },
+      ingredientAmounts: ['300 ml', '200 ml'],
+      shortDescription: 'Perfect fot sunny days',
+      additionalInfo: 'My favorite drink',
+      categories: { connect: { name: 'Clasic' } },
+      author: { connect: { username: 'Nicolas The Normie' } },
     },
   });
 }
